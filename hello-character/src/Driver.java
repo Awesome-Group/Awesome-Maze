726be0cc6115090318d243e61a9331ad029225c5
ericchase <ericnalley@gmail.com>
2017-08-05 19:52:42 -0500

    updated driver.java & added code style

--- hello-character/src/Driver.java
+++ hello-character/src/Driver.java
@@ -15,53 +15,48 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
+import com.jme3.system.AppSettings;
 
 public class Driver extends SimpleApplication implements ActionListener
 {
-    private Spatial sceneModel;
-    private BulletAppState bulletAppState;
-    private RigidBodyControl landscape;
-    private CharacterControl player;
-    private Vector3f walkDirection = new Vector3f();
-    private boolean left = false, right = false, up = false, down = false, run = false;
+    // public:
 
-    //Temporary vectors used on each frame.
-    //They here to avoid instanciating new vectors on each frame.
-    private Vector3f camDir = new Vector3f();
-    private Vector3f camLeft = new Vector3f();
-
-    private Vector3f xDirection = new Vector3f();
-    private Vector3f zDirection = new Vector3f();
-
-    private float walkSpeedFactor;
-    private float runSpeedFactor;
-    private float currentSpeedFactor;
 
     public static void main(String[] args)
     {
-        /*This is where the program starts and calls the start() method
-          which runs the simpleInitApp() method.
-        */
+        // This is jmonkey's settings class for the simple app.
+        // Normal
+        AppSettings settings = new AppSettings(true);
+        settings.put("Width", 1280);
+        settings.put("Height", 720);
+        settings.put("Title", "Awesome Maze");
+        settings.put("VSync", true);
+        // Anti-Aliasing
+        settings.put("Samples", 4);
+
+        // This is where the program starts and calls the start() method
+        //  which runs the simpleInitApp() method.
         Driver driver = new Driver();
+        driver.setSettings(settings);   // apply the settings object above to our driver
+        driver.setShowSettings(false);  // disable the settings windows from showing
         driver.start();
     }
 
+
+    @Override
     public void simpleInitApp()
     {
-        /** The BulletAppState gives our application access to physics features,
+        /* The BulletAppState gives our application access to physics features,
          *  such as collision detection. It is integerated by the jME's jBullet integeration
          *  which is an external physics engine. This piece of code is required in every
-         *  application that works with physics.*/
+         *  application that works with physics.
+         */
         bulletAppState = new BulletAppState();
         stateManager.attach(bulletAppState);
 
-
-        /** The debug feature shows us the wireframes of the mesh renderer.
-         *  This should be enabled for debugging only.*/
-        //bulletAppState.setDebugEnabled(true);
-
         /* These are the default run and walk speed factors. We also set the move speed according to
-        *  what mode are we on.*/
+         *  what mode are we on.
+         */
         runSpeedFactor = 0.6f;
         walkSpeedFactor = 0.3f;
         currentSpeedFactor = walkSpeedFactor;
@@ -80,8 +75,7 @@ public class Driver extends SimpleApplication implements ActionListener
 
         // We set up collision detection for the scene by creating a
         // compound collision shape and a static RigidBodyControl with mass zero.
-        CollisionShape sceneShape =
-                CollisionShapeFactory.createMeshShape(sceneModel);
+        CollisionShape sceneShape = CollisionShapeFactory.createMeshShape(sceneModel);
         landscape = new RigidBodyControl(sceneShape, 0);
         sceneModel.addControl(landscape);
 
@@ -99,69 +93,31 @@ public class Driver extends SimpleApplication implements ActionListener
         player.setPhysicsLocation(new Vector3f(0, 10, 0));
 
 
-        /*We attach the scene and the player to the rootnode and the physics space,
-        * to make them appear in the game world. Note that you have to add all kinds of physics
-        * control to the physics space for them to have physics.*/
+        /* We attach the scene and the player to the rootnode and the physics space,
+         *  to make them appear in the game world. Note that you have to add all kinds of physics
+         *  control to the physics space for them to have physics.
+         */
         rootNode.attachChild(sceneModel);
         bulletAppState.getPhysicsSpace().add(landscape);
         bulletAppState.getPhysicsSpace().add(player);
-        bulletAppState.setDebugEnabled(true);
+
+        /* The debug feature shows us the wireframes of the mesh renderer.
+         * This should be enabled for debugging only.
+         */
+        //bulletAppState.setDebugEnabled(true);
     }
 
-    private void setUpLight() {
-        // We add light so we see the scene
-        AmbientLight al = new AmbientLight();
-        al.setColor(ColorRGBA.White.mult(1.3f));
-        rootNode.addLight(al);
 
-        DirectionalLight dl = new DirectionalLight();
-        dl.setColor(ColorRGBA.White);
-        dl.setDirection(new Vector3f(2.8f, -2.8f, -2.8f).normalizeLocal());
-        rootNode.addLight(dl);
-    }
-
-    /** We over-write some navigational key mappings here, so we can
-     * add physics-controlled walking and jumping: */
-    private void setUpKeys() {
-        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
-        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
-        inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_W));
-        inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
-        inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
-        inputManager.addMapping("Run", new KeyTrigger(KeyInput.KEY_LSHIFT));
-        inputManager.addListener(this, "Left", "Right", "Up", "Down", "Jump", "Run");
-    }
-
-    /** These are our custom actions triggered by key presses.
-     * We do not walk yet, we just keep track of the direction the user pressed. */
-    public void onAction(String binding, boolean isPressed, float tpf) {
-        if (binding.equals("Left")) {
-            left = isPressed;
-        } else if (binding.equals("Right")) {
-            right= isPressed;
-        } else if (binding.equals("Up")) {
-            up = isPressed;
-        } else if (binding.equals("Down")) {
-            down = isPressed;
-        } else if (binding.equals("Jump")) {
-            // Jump functionality not determined yet
-            //if (isPressed) { player.jump(); }
-        } else if(binding.equals("Run")){
-            run = isPressed;
-        }
-    }
-
-    /**
-     * This is the main event loop--walking happens here.
+    /* This is the main event loop--walking happens here.
      * We check in which direction the player is walking by interpreting
      * the camera direction forward (camDir) and to the side (camLeft).
      * The setWalkDirection() command is what lets a physics-controlled player walk.
      * We also make sure here that the camera moves with player.
      */
-
     @Override
-    public void simpleUpdate(float tpf) {
-        if(run)
+    public void simpleUpdate(float tpf)
+    {
+        if (run)
         {
             currentSpeedFactor = runSpeedFactor;
         }
@@ -171,7 +127,8 @@ public class Driver extends SimpleApplication implements ActionListener
         }
 
         /* The up and down movement has been allowed only in the x and z direction because
-        * changing all direction would enable to player to jump awkwardly.*/
+         * changing all direction would enable to player to jump awkwardly.
+         */
         camDir.setX(cam.getDirection().getX() * currentSpeedFactor);
         camDir.setZ(cam.getDirection().getZ() * currentSpeedFactor);
         camDir.setY(0f);
@@ -179,18 +136,23 @@ public class Driver extends SimpleApplication implements ActionListener
         camLeft.set(cam.getLeft()).multLocal(currentSpeedFactor);
 
         /* The multiple if statements helps us determine which position the character wants to walk
-         * in. The walk direction is calculated based on the boolean values determined by the onAction method.*/
+         *  in. The walk direction is calculated based on the boolean values determined by the onAction method.
+         */
         walkDirection.set(0, 0, 0);
-        if (left) {
+        if (left)
+        {
             walkDirection.addLocal(camLeft);
         }
-        if (right) {
+        if (right)
+        {
             walkDirection.addLocal(camLeft.negate());
         }
-        if (up) {
+        if (up)
+        {
             walkDirection.addLocal(camDir);
         }
-        if (down) {
+        if (down)
+        {
             walkDirection.addLocal(camDir.negate());
         }
 
@@ -203,8 +165,104 @@ public class Driver extends SimpleApplication implements ActionListener
 
         xDirection.set(0f, cam.getDirection().getY(), 0f);
         zDirection.set(0f, sceneModel.getLocalTranslation().getY(), 0f);
-        System.out.println(cam.getDirection().getX());
+        //System.out.println(cam.getDirection().getX());
         //System.out.println(cam.getLocation());
     }
+
+
+    /* These are our custom actions triggered by key presses.
+     * We do not walk yet, we just keep track of the direction the user pressed.
+     */
+
+    /**
+     * Called when an input to which this listener is registered to is invoked.
+     *
+     * @param name      The name of the mapping that was invoked
+     * @param isPressed True if the action is "pressed", false otherwise
+     * @param tpf       The time per frame value.
+     */
+    @Override
+    public void onAction(String name, boolean isPressed, float tpf)
+    {
+        if (name.equals("Left"))
+        {
+            left = isPressed;
+        }
+        else
+            if (name.equals("Right"))
+            {
+                right = isPressed;
+            }
+            else
+                if (name.equals("Up"))
+                {
+                    up = isPressed;
+                }
+                else
+                    if (name.equals("Down"))
+                    {
+                        down = isPressed;
+                    }
+                    else
+                        if (name.equals("Jump"))
+                        {
+                            // Jump functionality not determined yet
+                            //if (isPressed) { player.jump(); }
+                        }
+                        else
+                            if (name.equals("Run"))
+                            {
+                                run = isPressed;
+                            }
+    }
+
+    // private:
+
+    private void setUpLight()
+    {
+        // We add light so we see the scene
+        AmbientLight al = new AmbientLight();
+        al.setColor(ColorRGBA.White.mult(1.3f));
+        rootNode.addLight(al);
+
+        DirectionalLight dl = new DirectionalLight();
+        dl.setColor(ColorRGBA.White);
+        dl.setDirection(new Vector3f(2.8f, -2.8f, -2.8f).normalizeLocal());
+        rootNode.addLight(dl);
+    }
+
+    /* We over-write some navigational key mappings here, so we can
+     *  add physics-controlled walking and jumping:
+     */
+    private void setUpKeys()
+    {
+        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
+        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
+        inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_W));
+        inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
+        inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
+        inputManager.addMapping("Run", new KeyTrigger(KeyInput.KEY_LSHIFT));
+        inputManager.addListener(this, "Left", "Right", "Up", "Down", "Jump", "Run");
+    }
+
+
+    private Spatial sceneModel;
+    private BulletAppState bulletAppState;
+    private RigidBodyControl landscape;
+    private CharacterControl player;
+    private Vector3f walkDirection = new Vector3f();
+    private boolean left = false, right = false, up = false, down = false, run = false;
+
+    //Temporary vectors used on each frame.
+    //They here to avoid instanciating new vectors on each frame.
+    private Vector3f camDir = new Vector3f();
+    private Vector3f camLeft = new Vector3f();
+
+    private Vector3f xDirection = new Vector3f();
+    private Vector3f zDirection = new Vector3f();
+
+    private float walkSpeedFactor;
+    private float runSpeedFactor;
+    private float currentSpeedFactor;
 
 }
